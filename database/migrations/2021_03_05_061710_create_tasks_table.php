d7e8c6e507345978ce6d542901fa8b627e206fc8
JITHUJOY <jithujoyjj1@gmail.com>
2021-03-05 13:28:56 +0530

    changed task table

--- database/migrations/2021_03_05_061710_create_tasks_table.php
+++ database/migrations/2021_03_05_061710_create_tasks_table.php
@@ -18,7 +18,7 @@ class CreateTasksTable extends Migration
             $table->integer('user_id');
             $table->string('title',25);
             $table->text('body');
-            $table->boolean('status');
+            $table->string('status',25);
             $table->timestamps();
         });
     }
